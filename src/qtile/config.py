102b010365172ba436e3901fdf9a3bcbf3cbb03b
axju <moin@axju.de>
2022-07-14 23:00:07 +0200

    qtile

--- src/qtile/config.py
+++ src/qtile/config.py
@@ -1,29 +1,3 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from libqtile import bar, layout, widget, extension
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
@@ -33,46 +7,36 @@ mod = "mod4"
 terminal = guess_terminal()
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
     Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
+    
     Key([mod, "shift"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+    
     Key([mod, "control"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
     Key([mod, "control"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
+    
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    # Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 
-    Key([mod, "control"], "Return", lazy.run_extension(extension.DmenuRun(
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
+    Key([mod, "control"], "Return",
+        lazy.spawn("rofi -show drun"),
+        desc="Launch Rofi menu"
+    ),
+
+    Key([mod, "shift"], "Return", lazy.run_extension(extension.DmenuRun(
         dmenu_prompt=">",
         #dmenu_font="Andika-8",
         dmenu_bottom=False,
@@ -82,48 +46,15 @@ keys = [
         selected_background="#079822",
         selected_foreground="#fff",
         #dmenu_height=24,  # Only supported by some dmenu forks
-    ))),
-
-    Key([mod], "space",
-        lazy.spawn("rofi -show drun"),
-        desc="Launch Rofi menu"
-    ),
+    ))),    
 
     Key([mod, "mod1"], "Left", lazy.to_screen(0)),
     Key([mod, "mod1"], "Right", lazy.to_screen(1)),
-
 ]
-
-#groups = [Group(i) for i in "123456789"]
-
-#for i in groups:
-#    keys.extend(
-#        [
-#            # mod1 + letter of group = switch to group
-#            Key(
-#                [mod],
-#                i.name,
-#                lazy.group[i.name].toscreen(),
-#                desc="Switch to group {}".format(i.name),
-#            ),
-#            # mod1 + shift + letter of group = switch to & move focused window to group
-#            Key(
-#                [mod, "shift"],
-#                i.name,
-#                lazy.window.togroup(i.name, switch_group=True),
-#                desc="Switch to & move focused window to group {}".format(i.name),
-#            ),
-#            # Or, use below if you prefer not to switch to that group.
-#            # # mod1 + shift + letter of group = move focused window to group
-#            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-#            #     desc="move focused window to group {}".format(i.name)),
-#        ]
-#    )
 
 
 groups = [Group("DEV", layout='monadtall'),
           Group("WWW", layout='monadtall'),
-          Group("SYS", layout='monadtall'),
           Group("SYS", layout='monadtall'),
           Group("DOC", layout='monadtall'),
           Group("VBOX", layout='monadtall'),
@@ -133,8 +64,25 @@ groups = [Group("DEV", layout='monadtall'),
           Group("GFX", layout='floating')]
 
 
-from libqtile.dgroups import simple_key_binder
-dgroups_key_binder = simple_key_binder("mod4")
+for no, i in enumerate(groups, 1):
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+               [mod],
+               str(no),
+               lazy.group[i.name].toscreen(),
+               desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                str(no),
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+        ]
+    )
 
 
 layout_theme = {"border_width": 2,
@@ -142,7 +90,6 @@ layout_theme = {"border_width": 2,
                 "border_focus": "e1acff",
                 "border_normal": "1D2330"
                 }
-
 
 
 layouts = [
@@ -155,7 +102,7 @@ layouts = [
     layout.MonadTall(**layout_theme),
     layout.Max(**layout_theme),
     layout.Stack(**layout_theme),
-    layout.RatioTile(**layout_theme),
+    #layout.RatioTile(**layout_theme),
     #layout.Tile(**layout_theme),
     #layout.TreeTab(**layout_theme),
     #layout.VerticalTile(**layout_theme),
@@ -174,9 +121,7 @@ screens = [
     Screen(
         top=bar.Bar(
             [
-                # widget.CurrentLayout(),
                 widget.GroupBox(),
-                # widget.Prompt(),
                 widget.WindowName(),
                 widget.Chord(
                     chords_colors={
@@ -186,33 +131,16 @@ screens = [
                 ),
                 widget.Systray(),
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                #widget.QuickExit(),
             ],
             40,
-            #border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            #border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
     Screen(
         top=bar.Bar(
             [
-        #        # widget.CurrentLayout(),
                 widget.GroupBox(),
-        #        # widget.Prompt(),
-        #        widget.WindowName(),
-        #        #widget.Chord(
-        #        #    chords_colors={
-        #        #        "launch": ("#ff0000", "#ffffff"),
-        #        #    },
-        #        #    name_transform=lambda name: name.upper(),
-        #        #),
-        #        #widget.Systray(),
-                #widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-        #        #widget.QuickExit(),
             ],
             40,
-        #    #border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-        #    #border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
 
